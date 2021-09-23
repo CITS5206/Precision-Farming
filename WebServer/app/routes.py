dbce525f2679f0d2fe29b1134a5c6aaee93305c5
Harper <haarperwu@gmail.com>
2021-09-23 12:29:33 +0800

    edit tracking.html to take argument from routes.py

--- WebServer/app/routes.py
+++ WebServer/app/routes.py
@@ -4,4 +4,16 @@ from flask  import render_template
 @app.route('/')
 @app.route('/tracking')
 def index():
-    return render_template('tracking.html')
+    # TODO: func: python - device-status
+    sensor = 'up'
+    gps = 'down'
+    # TODO: func: get A, B coordinate
+    pointA = '-40.678901, 163.8765432'
+    pointB = '-40.678901, 163.8765432'
+    # TODO: func: python - get current-location
+    currentLat = '-31.980945378411835'
+    currentLong = '115.81819424545051'
+    return render_template('tracking.html', 
+                            sensor=sensor, gps=gps,
+                            pointA=pointA, pointB=pointB,
+                            currentLat=currentLat, currentLong=currentLong)
--- WebServer/app/static/app.js
+++ WebServer/app/static/app.js
@@ -3,8 +3,10 @@ function tracking() {
     let btn_track =  document.getElementById('btn-track')
     if (btn_track.innerHTML == 'Start Tracking'){
         btn_track.innerHTML = 'Stop Tracking'
+        // TODO: func: js read json, draw lines and current location
     } else {
         btn_track.innerHTML = 'Start Tracking'
+        // TODO: Stop function drawing
     }
-
+    
 }
