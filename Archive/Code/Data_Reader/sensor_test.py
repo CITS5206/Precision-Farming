e9f3c1ae9b12f4c4ca51e96b71fa01b80c8d3ffd
draj2931 <draj293@gmail.com>
2021-09-24 20:08:06 +0800

    Format changes

--- Archive/Code/Data_Reader/sensor_test.py
+++ Archive/Code/Data_Reader/sensor_test.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import unittest
 from reader import readSensor
+from sensorCSV import creatCSVfile
 
 class TestSensorReader(unittest.TestCase):
 
@@ -10,17 +11,38 @@ class TestSensorReader(unittest.TestCase):
     def test_checkfile(self):
         
 
-        with open("./Data_Reader/test.txt",'a') as outfile:
+        with open("./Archive/Code/Data_Reader/test.txt",'a') as outfile:
             outfile.write("Test")
         temp =  readSensor()
         
-        self.assertEqual(temp.check_file_exist("./Data_Reader/test.txt"),True)
-        self.assertEqual(temp.check_file_exist("./Data_Reader/test123.txt"),False)
+        self.assertEqual(temp.check_file_exist("./Archive/Code/Data_Reader/test.txt"),True)
+        self.assertEqual(temp.check_file_exist("./Archive/Code/Data_Reader/test123.txt"),False)
 
-        os.remove("./Data_Reader/test.txt")
+        os.remove("./Archive/Code/Data_Reader/test.txt")
+
+# Test case to check csv file creation in csv path
+        
+    def test_createcsv(self):
+        temp1 = creatCSVfile()
+        temp1.readtxtfile()
+        date=str(datetime.date.today())
+        val=os.path.isfile("./Archive/Code/Data_Reader/CSVfile/DUALEMdata"+date+".csv")
+        val1=os.path.isfile("./Archive/Code/Data_Reader/CSVfile/GPSdata"+date+".csv")
+        val2=os.path.isfile("./Archive/Code/Data_Reader/CSVfile/Metadata"+date+".csv")
+        self.assertEquals(val1,True)
+        self.assertEquals(val2,True)
+        self.assertEquals(val,True)
+        
+        if val1 and val and val:
+            os.remove("./Archive/Code/Data_Reader/CSVfile/GPSdata"+date+".csv")
+            os.remove("./Archive/Code/Data_Reader/CSVfile/DUALEMdata"+date+".csv")
+            os.remove("./Archive/Code/Data_Reader/CSVfile/Metadata"+date+".csv")
+
+
 
     
-        
+
+    
 
 if __name__ == '__main__':
     unittest.main()
