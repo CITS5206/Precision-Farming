8e9d47066f95db038725cd8b67e3002f6ed803ac
draj2931 <draj293@gmail.com>
2021-09-24 20:09:52 +0800

    Comment added

--- Archive/Code/Data_Reader/main.py
+++ Archive/Code/Data_Reader/main.py
@@ -10,8 +10,8 @@ if __name__ == "main":
 
 
 
-    #check the values of port and validate 
-
+    #checks the input values of port and validate them 
+    # Assigns the baurd rate for the function
 
     if(dualport != None and GPSport != None and GPSport == '/dev/tty.usbserial-AL02V3VW'): 
         gpsbaudrate=38400
@@ -23,6 +23,10 @@ if __name__ == "main":
     sensor_obj = readSensor()
     csv_file=creatCSVfile()
 
+    # Toggles betweent the two class to read the values from two sensor at time 
+    # Sensor read value for 4 second and sleeps where gps reads for a second and sleeps 
+    # The process will be killed by user in stop reading command from user in GUI 
+    
     while True:
         while FLAG:
             #do this for 4 seconds
