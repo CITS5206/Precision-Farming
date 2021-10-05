5442f2af4cca335925e350a33b1e80346fddad55
Harper <haarperwu@gmail.com>
2021-10-05 16:19:29 +0800

    Edited Arjun's Generate-JSON for testing; Edited getJson function

--- WebServer/app/__init__.py
+++ WebServer/app/__init__.py
@@ -2,8 +2,10 @@ from flask              import Flask
 from config             import Config
 from flask_sqlalchemy   import SQLAlchemy
 from flask_migrate      import Migrate
+from flask_jsglue       import JSGlue
 
 app = Flask(__name__)
+jsglue = JSGlue(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- WebServer/app/routes.py
+++ WebServer/app/routes.py
@@ -12,19 +12,20 @@ def tracking(mapid):
     pointA = '-30.678901, 114.8765432'
     pointB = '-32.678901, 116.8765432'
     # TODO: func: python - get current-location
+    # uwa
     currentLat = '-31.980945378411835'
     currentLong = '115.81819424545051'
     map = Map.query.filter_by(id = mapid).first_or_404()
     return render_template('tracking.html', title='PrecisionFarming-Tracking', 
                             sensor=sensor, gps=gps,
                             pointA=pointA, pointB=pointB,
-                            currentLat=currentLat, currentLong=currentLong,
+                            currentLat = currentLat, currentLong =currentLong,
                             map=map)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    mapLst = Map.query.all()
+    # mapLst = Map.query.all()
     return render_template('index.html', title='PrecisionFarming-Home')
 
 @app.route('/history')
@@ -38,15 +39,14 @@ def map():
     return render_template('map.html', title='PrecisionFarming-Map', lstMap=lstMap)
 
 @app.route('/getJson')
-def getJson(path='./out2.json'):
-    myfile = open(path, 'r')
-    data = myfile.read()
-    polylist = []
-    datajson = json.loads(data)
-    for key in datajson:
-        latlonglist = [0,0]
-        latlonglist[0] = key
-        latlonglist[1] = datajson[key]['LONd']
-        polylist.append(latlonglist)
-    polylistjson = json.dumps(polylist)
-    return polylistjson
+# def getJson(path='./out2.json'):
+def getJson(path='./Generate-JSON/gps-data.json'):
+    f = open(path, 'r')
+    data = json.load(f)
+    return data
+
+@app.route('/finishTrack/<mapid>')
+def finishTrack(mapid):
+    # TODO: Save csv file
+    # TODO: update map name and csv file name into history table
+    return render_template('history.html', title='History')
