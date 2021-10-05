fb1ceca8aa7e57f1793e52bc81d7c46081885a15
Harper <haarperwu@gmail.com>
2021-10-05 22:18:59 +0800

    Add get current position from json; Remove history related content;

--- WebServer/app/routes.py
+++ WebServer/app/routes.py
@@ -1,5 +1,5 @@
 from app        import app, db
-from flask      import render_template
+from flask      import render_template, redirect, url_for
 from app.models import Map
 import json
 
@@ -12,41 +12,27 @@ def tracking(mapid):
     pointA = '-30.678901, 114.8765432'
     pointB = '-32.678901, 116.8765432'
     # TODO: func: python - get current-location
-    # uwa
-    currentLat = '-31.980945378411835'
-    currentLong = '115.81819424545051'
+    currPos = getJson()['CURRENT_POS']
     map = Map.query.filter_by(id = mapid).first_or_404()
     return render_template('tracking.html', title='PrecisionFarming-Tracking', 
                             sensor=sensor, gps=gps,
                             pointA=pointA, pointB=pointB,
-                            currentLat = currentLat, currentLong =currentLong,
+                            currPos = currPos,
                             map=map)
 
-@app.route('/')
+
 @app.route('/index')
 def index():
-    # mapLst = Map.query.all()
     return render_template('index.html', title='PrecisionFarming-Home')
 
-@app.route('/history')
-def history():
-    # lstHistory = History.query.all()
-    return render_template('history.html', title='History')
-
+@app.route('/')
 @app.route('/map')
 def map():
     lstMap = Map.query.all()
     return render_template('map.html', title='PrecisionFarming-Map', lstMap=lstMap)
 
 @app.route('/getJson')
-# def getJson(path='./out2.json'):
 def getJson(path='./Generate-JSON/gps-data.json'):
     f = open(path, 'r')
     data = json.load(f)
     return data
-
-@app.route('/finishTrack/<mapid>')
-def finishTrack(mapid):
-    # TODO: Save csv file
-    # TODO: update map name and csv file name into history table
-    return render_template('history.html', title='History')
