19aa7eec342d8a5fba6dafb7e1fe516a4a092a21
Harper <haarperwu@gmail.com>
2021-10-06 23:52:27 +0800

    Edit json file key name

--- WebServer/app/routes.py
+++ WebServer/app/routes.py
@@ -6,17 +6,17 @@ import json
 @app.route('/tracking/<mapid>')
 def tracking(mapid):
     # TODO: func: python - device-status
-    sensor = 'up'
-    gps = 'down'
-    # TODO: func: get A, B coordinate
-    pointA = '-30.678901, 114.8765432'
-    pointB = '-32.678901, 116.8765432'
+    # sensor = 'up'
+    # gps = 'down'
+    # # TODO: func: get A, B coordinate
+    # pointA = '-30.678901, 114.8765432'
+    # pointB = '-32.678901, 116.8765432'
     # TODO: func: python - get current-location
-    currPos = getJson()['CURRENT_POS']
+    currPos = getJson()['live']
     map = Map.query.filter_by(id = mapid).first_or_404()
     return render_template('tracking.html', title='PrecisionFarming-Tracking', 
-                            sensor=sensor, gps=gps,
-                            pointA=pointA, pointB=pointB,
+                            # sensor=sensor, gps=gps,
+                            # pointA=pointA, pointB=pointB,
                             currPos = currPos,
                             map=map)
 
@@ -32,7 +32,7 @@ def map():
     return render_template('map.html', title='PrecisionFarming-Map', lstMap=lstMap)
 
 @app.route('/getJson')
-def getJson(path='./Generate-JSON/gps-data.json'):
+def getJson(path='./leaflet-demo/d.json'):
     f = open(path, 'r')
     data = json.load(f)
     return data
