2ea43bffa20da7a8921d077a299212fa95ad5fc4
Harper-Wu <haarperwu@gmail.com>
2021-10-12 00:02:02 +0800

    Add heatmap; map list use listdir to replace database

--- WebServer/app/routes.py
+++ WebServer/app/routes.py
@@ -1,10 +1,11 @@
-from app        import app, db
-from flask      import render_template, redirect, url_for
+from app        import app
+from flask      import render_template, redirect, url_for, request
 from app.models import Map
 import json
+import os
 
-@app.route('/tracking/<mapid>')
-def tracking(mapid):
+@app.route('/tracking/')
+def tracking():
     # TODO: func: python - device-status
     # sensor = 'up'
     # gps = 'down'
@@ -12,23 +13,30 @@ def tracking(mapid):
     # pointA = '-30.678901, 114.8765432'
     # pointB = '-32.678901, 116.8765432'
     # TODO: func: python - get current-location
+    # currPos = [-31.977397621666668,115.81710717183333]
+    mapName = request.args.get('mapName')
+    measure = request.args.get('measure')
     currPos = getJson()['live']
-    map = Map.query.filter_by(id = mapid).first_or_404()
+    # map = Map.query.filter_by(id = mapid).first_or_404()
     return render_template('tracking.html', title='PrecisionFarming-Tracking', 
                             # sensor=sensor, gps=gps,
                             # pointA=pointA, pointB=pointB,
-                            currPos = currPos,
-                            map=map)
+                            measure = measure, currPos = currPos, mapName=mapName)
 
 
 @app.route('/index')
 def index():
     return render_template('index.html', title='PrecisionFarming-Home')
 
+
+@app.route('/heatmap')
+def heatmap():
+    return render_template('heatmap.html', title='Heatmap')
+
 @app.route('/')
 @app.route('/map')
 def map():
-    lstMap = Map.query.all()
+    lstMap = getMapNames()
     return render_template('map.html', title='PrecisionFarming-Map', lstMap=lstMap)
 
 @app.route('/getJson')
@@ -36,3 +44,12 @@ def getJson(path='./leaflet-demo/d.json'):
     f = open(path, 'r')
     data = json.load(f)
     return data
+
+def getMapNames():
+    maps = []
+    # print(os.getcwd())
+    entries = os.listdir('./app/static/maps')
+    for e in entries:
+        if not e.startswith('.'):
+            maps.append(e)
+    return maps
