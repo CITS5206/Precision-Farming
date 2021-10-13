d4728b1ac772fb3e968e4c2edcd2d0f8491ba3c5
clariza look <clarizlook.tech@gmail.com>
2021-10-13 14:15:37 +0800

    leaflet update
    leaflet update

---
+++ WebServer/leaflet/main.py
@@ -0,0 +1,23 @@
+import threading
+from gpsSensor import gpsSensor
+
+#dualem = Sensor(port=8888,baudrate=9600,sensorType='soil')
+#gps = gpsSensor(port="/dev/tty.usbserial-1110",baudrate=9600,timeout=1 ,mode='prod',filename='dualem.txt')
+gps2 = gpsSensor(port="8882",baudrate=38400,timeout=1 ,mode='debug',filename='d.json')
+
+#dualem.dataout()
+#gps_task_1 = threading.Thread(target=gps.readData)
+gps_task_2 = threading.Thread(target=gps2.readData)
+
+#gps_task_1.start()
+gps_task_2.start()
+
+#gps_task_1.join()
+
+gps_task_2.join()
+print("END OF PROGRAM")
+
+
+
+
+
