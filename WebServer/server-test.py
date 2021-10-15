8c7c85bed4c9b437134f936802c228338dea5d4f
clariza look <clarizlook.tech@gmail.com>
2021-10-15 22:38:40 +0800

    Updated the server-test.py
    Updated server-test.py with test on if the there is no file inside the 'map' folder on the 'map' page, added

--- WebServer/server-test.py
+++ WebServer/server-test.py
@@ -2,11 +2,18 @@
    This is a unit test for the web server
      - it should test if 'map' page (or '/') and '/tracking' page are loading properly (are returning to 200 status code)
      - it should test if other pages that is not map, tracking and '/' are serving a 404 message
+     - it should test if the there is no file inside the 'map' folder on the 'map' page
 
-   Author: Clariza Look
-   Date Written: 15-Oct-2021
    
-   How to use: 
+ The University of Western Australia : 2021
+ CITS5206 Professional Computing
+ Group: Precision Farming
+ Source Code
+ Author: Clariza Look
+ Date Created: 15-Oct-2021
+ Version: 1.0
+ State : Stable
+ How to use: 
      - python3 server-test.py
 """
 
@@ -15,8 +22,22 @@ from app import app
 
 class BasicTestCase(unittest.TestCase):
 
+    # Tests if map page's map is empty
+    # If map folder is empty, the page displays something like "please place map folders" 
+    # This test captures that message and 
+    # check if message is not found (assertTrue), then returns OK
+    def test_map_is_empty_on_map_page(self):
+            
+            tester = app.test_client(self)
+            response = tester.get('/map', content_type='html/text')
+            self.assertTrue(b'please place map folders', response.data) #the map folder is NOT empty
+            
+            #self.assertTrue(b'please place map folders', response.data) #the map folder is empty (this line to be removed)
+        
     
-    # Ensure that map page loads correctly 
+    # Ensure that map page loads correctly
+    # If page loads correctly, the page displays something like "Map" 
+    # Then test returns OK
     def test_map_page_loads(self):
            
             tester = app.test_client(self)
@@ -28,6 +49,7 @@ class BasicTestCase(unittest.TestCase):
 
     # Check if page is not part of the website (we only have 3 pages) 
     # If the page is not part of the 3, that page should return a 404 error status code
+    # Then test runs correctly, it should returns OK
     def test_404pages(self):
             tester = app.test_client(self)
             pages = ['/', '/map', '/heatmap', 'tracking'] # home page and /map is the same page
@@ -61,6 +83,6 @@ class BasicTestCase(unittest.TestCase):
                     response = tester.get(page, content_type='html/text')
                     self.assertEqual(response.status_code, 200)                                       
 
+
 if __name__ == '__main__':
     unittest.main()
-    
