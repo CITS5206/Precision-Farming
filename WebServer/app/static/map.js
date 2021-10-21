0ae45268cd5b47258234b1a513d199aa762447a6
Harper-Wu <haarperwu@gmail.com>
2021-10-21 15:25:54 +0800

    Separate live track map and heatmap

--- WebServer/app/static/map.js
+++ WebServer/app/static/map.js
@@ -41,72 +41,26 @@ var markerLayer = L.circle(currPos, {
 var eleCurrPos = document.getElementById('current-position');
 var resJson
 
-var cfg = {
-    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    "radius": 15,
-    "maxOpacity": .8, 
-    // scales the radius based on map zoom
-    "scaleRadius": false, 
-    // if set to false the heatmap uses the global maximum for colorization
-    // if activated: uses the data maximum within the current map boundaries 
-    //   (there will always be a red spot with useLocalExtremas true)
-    "useLocalExtrema": true,
-    // which field name in your data represents the latitude - default "lat"
-    latField: 'lat',
-    // which field name in your data represents the longitude - default "lng"
-    lngField: 'lng',
-    // // which field name in your data represents the data value - default "value"
-    valueField: measure
-    // TODO: explain meaning / color of heatmap
-};
-
-var heatmapLayer = new HeatmapOverlay(cfg).addTo(map);
-
 var getJson = async (callback) => {
     var res = await fetch('/getJson', {cache: "no-cache"});
     resJson = await res.json();
-    currPos = resJson['live'];
+    currPos = resJson['live']
+    latlngs = resJson['LatLongs']
+    console.log(latlngs)
     callback();
 };
 
 function draw() {
     getJson(function () {
         markerLayer.setLatLng(currPos);
-        heatmapLayer.setData(resJson);
+        polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
     })
 };
 
 draw_Interval = setInterval(draw, 250);
 
-function getYMD(){
-    var d = new Date(),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('');
-}
-
-
-$('#saveImage').click(async function () {
-    var saveImage = document.getElementById('saveImage');
-    if (saveImage.innerHTML ==='Stop and Save'){
-        map.setZoom(18);
-        await new Promise(r => setTimeout(r, 2000));        
-        blob = domtoimage.toBlob(document.getElementById('map'))
-            .then(function (blob) {
-                let ymd = getYMD();
-                // download path depend on the browser setting, javascript can not specify
-                // File can not be saved when page has GET error,
-                // like 'http://localhost:5000/blah.png 404 (NOT FOUND)'
-                window.saveAs(blob, mapName+'_'+measure+'_'+ymd+'.png');
-            });
-        clearInterval(draw_Interval);
-        saveImage.innerHTML = 'Done';
-    }
+$('#stop').click(function () {
+    clearInterval(draw_Interval);
+    document.getElementById('stop').innerHTML = 'Stoped';
+    
 })
