46d892052c021d918a6638c46888e7aa3fbac481
Arjun Panicker <Panicker.arjun21@gmail.com>
2021-10-21 18:04:51 +0800

    Updated Server and Main Code

--- Web Server/app/__init__.py
+++ Web Server/app/__init__.py
@@ -23,13 +23,11 @@
 '''
 
 from flask              import Flask
-from config             import Config
 from flask_jsglue       import JSGlue
 
 app = Flask(__name__)
 jsglue = JSGlue(app)
-app.config.from_object(Config)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 
 from app import routes
--- Web Server/server.py
+++ Web Server/server.py
@@ -23,4 +23,6 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000) 
+    app.run(debug=True, host="0.0.0.0", port=3152) 
+    # app.run(debug=True) 
+
